bb4068f3b0a66d78a82d224bcefabc847ff54909
jose_ <jose_@DESKTOP-J1G74PJ>
2017-01-07 13:58:48 +0000

    Apresenta a DataTable de pesquisa

--- MADKOUA_CLIENTE/Form1.Designer.cs
+++ MADKOUA_CLIENTE/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.PanelLeft = new System.Windows.Forms.Panel();
             this.PanelDGV = new System.Windows.Forms.Panel();
-            this.PanelTop = new System.Windows.Forms.Panel();
             this.DGVLivros = new System.Windows.Forms.DataGridView();
+            this.PanelTop = new System.Windows.Forms.Panel();
             this.TBPesquisa = new System.Windows.Forms.TextBox();
             this.PanelDGV.SuspendLayout();
-            this.PanelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGVLivros)).BeginInit();
+            this.PanelTop.SuspendLayout();
             this.SuspendLayout();
             // 
             // PanelLeft
@@ -43,64 +43,72 @@
             this.PanelLeft.BackColor = System.Drawing.Color.LightSteelBlue;
             this.PanelLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.PanelLeft.Location = new System.Drawing.Point(0, 0);
+            this.PanelLeft.Margin = new System.Windows.Forms.Padding(2);
             this.PanelLeft.Name = "PanelLeft";
-            this.PanelLeft.Size = new System.Drawing.Size(71, 688);
+            this.PanelLeft.Size = new System.Drawing.Size(53, 559);
             this.PanelLeft.TabIndex = 0;
             // 
             // PanelDGV
             // 
             this.PanelDGV.Controls.Add(this.DGVLivros);
-            this.PanelDGV.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PanelDGV.Location = new System.Drawing.Point(71, 0);
+            this.PanelDGV.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PanelDGV.Location = new System.Drawing.Point(53, 141);
+            this.PanelDGV.Margin = new System.Windows.Forms.Padding(2);
             this.PanelDGV.Name = "PanelDGV";
-            this.PanelDGV.Size = new System.Drawing.Size(1104, 688);
+            this.PanelDGV.Size = new System.Drawing.Size(828, 418);
             this.PanelDGV.TabIndex = 1;
-            // 
-            // PanelTop
-            // 
-            this.PanelTop.BackColor = System.Drawing.Color.Lavender;
-            this.PanelTop.Controls.Add(this.TBPesquisa);
-            this.PanelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelTop.Location = new System.Drawing.Point(71, 0);
-            this.PanelTop.Name = "PanelTop";
-            this.PanelTop.Size = new System.Drawing.Size(1104, 169);
-            this.PanelTop.TabIndex = 2;
             // 
             // DGVLivros
             // 
             this.DGVLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DGVLivros.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DGVLivros.Location = new System.Drawing.Point(0, 0);
+            this.DGVLivros.Margin = new System.Windows.Forms.Padding(2);
             this.DGVLivros.Name = "DGVLivros";
             this.DGVLivros.RowTemplate.Height = 24;
-            this.DGVLivros.Size = new System.Drawing.Size(1104, 688);
+            this.DGVLivros.Size = new System.Drawing.Size(828, 418);
             this.DGVLivros.TabIndex = 0;
+            // 
+            // PanelTop
+            // 
+            this.PanelTop.BackColor = System.Drawing.Color.Lavender;
+            this.PanelTop.Controls.Add(this.TBPesquisa);
+            this.PanelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelTop.Location = new System.Drawing.Point(53, 0);
+            this.PanelTop.Margin = new System.Windows.Forms.Padding(2);
+            this.PanelTop.Name = "PanelTop";
+            this.PanelTop.Size = new System.Drawing.Size(828, 137);
+            this.PanelTop.TabIndex = 2;
             // 
             // TBPesquisa
             // 
             this.TBPesquisa.Dock = System.Windows.Forms.DockStyle.Top;
             this.TBPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TBPesquisa.Location = new System.Drawing.Point(0, 0);
+            this.TBPesquisa.Margin = new System.Windows.Forms.Padding(2);
             this.TBPesquisa.Multiline = true;
             this.TBPesquisa.Name = "TBPesquisa";
-            this.TBPesquisa.Size = new System.Drawing.Size(1104, 30);
+            this.TBPesquisa.Size = new System.Drawing.Size(828, 25);
             this.TBPesquisa.TabIndex = 0;
+            this.TBPesquisa.TextChanged += new System.EventHandler(this.TBPesquisa_TextChanged);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1175, 688);
+            this.ClientSize = new System.Drawing.Size(881, 559);
             this.Controls.Add(this.PanelTop);
             this.Controls.Add(this.PanelDGV);
             this.Controls.Add(this.PanelLeft);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "MADKOUA Cliente";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.PanelDGV.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVLivros)).EndInit();
             this.PanelTop.ResumeLayout(false);
             this.PanelTop.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DGVLivros)).EndInit();
             this.ResumeLayout(false);
 
         }
--- MADKOUA_CLIENTE/Form1.cs
+++ MADKOUA_CLIENTE/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MADKOUA;
 
 namespace MADKOUA_CLIENTE
 {
@@ -17,5 +18,49 @@ namespace MADKOUA_CLIENTE
             InitializeComponent();
             
         }
+
+        DataTable tabela;
+        DataTable livros;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            livros = Livro.ListaLivros("Titulo", TBPesquisa.Text);
+            tabela = new DataTable();
+
+            tabela.Columns.Add("Titulo",typeof(String));
+            tabela.Columns.Add("Autor", typeof(String));
+            tabela.Columns.Add("Editora", typeof(String));
+
+            apresenta(TBPesquisa.Text);
+            
+        }
+
+        private void apresenta(String pesquisa)
+        {
+            tabela.Clear();
+            livros = Livro.ListaLivros("Titulo", TBPesquisa.Text);
+            Autor autor = new Autor();
+            Editora editora = new Editora();
+
+            foreach (DataRow linha in livros.Rows)
+            {                
+                autor.ID = linha.Field<int>("Autor_ID");                
+                editora.ID = linha.Field<int>("Editora_ID");
+                tabela.Rows.Add(linha.Field<String>("Titulo"), autor.Nome + " " + autor.Apelido, editora.Nome);
+            }
+            DGVLivros.DataSource = tabela;
+
+        }
+
+        private void TBPesquisa_TextChanged(object sender, EventArgs e)
+        {
+
+            apresenta(TBPesquisa.Text);
+                                  
+            
+
+        }
+
+        
     }
 }
